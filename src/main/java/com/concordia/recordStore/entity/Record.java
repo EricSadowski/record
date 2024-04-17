384351871ddbd3990da8d19d8549ded462957439
EricSadowski <118220751+EricSadowski@users.noreply.github.com>
2024-04-17 11:58:24 -0400

    1 more

--- src/main/java/com/concordia/recordStore/entity/Record.java
+++ src/main/java/com/concordia/recordStore/entity/Record.java
@@ -1,11 +1,11 @@
 package com.concordia.recordStore.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
 
 //@Entity
 //@Table(name="record")
